184fdfdade486532206a76622fab2802f46b1ff9
dongjilo <121302078+dongjilo@users.noreply.github.com>
2023-09-07 02:07:50 +0800

    reworked form.php

--- form.php
+++ form.php
@@ -9,114 +9,110 @@ $fruitSess =  $_SESSION['fruits'];
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <script src=""></script>
     <title>Fruit Manager</title>
-
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <script src="./js/bootstrap.bundle.min.js"></script>
     <style>
         body {
             padding-top: 70px;
             padding-bottom: 70px;
+            background-color: #eee;
         }
 
+        .card {
+            max-width: 500px;
+            margin: 0 auto;
+        }
     </style>
-
 </head>
 <body>
-<div class="container">
+<div class="container mt-5">
     <div class="row">
-        <!-- Select Fruits Card -->
-        <div class="col-sm-4">
-            <div class="card w-100">
+        <div class="col-md-12">
+            <div class="card p-4">
                 <div class="card-body">
-                    <h3 class="card-title">Select Fruits</h3>
-                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                        <label for="selFruits">Select Fruits here</label>
-                        <select name="selFruits" id="selFruits" class="form-control">
-                            <option value=""></option>
-                            <?php
-                            foreach ($fruitSess as $fruit) {
-                                echo "<option value='$fruit'>$fruit</option>";
-                            }
-                            ?>
-                        </select>
-                        <br>
-                        <input type="submit" value="Select" class="btn btn-success">
+                    <h3 class="card-title">Manage Fruits</h3>
+                    <form action="<?php $_PHP_SELF ?>" method="post">
+                        <div class="form-floating my-2">
+                            <select name="selFruits" id="selFruits" class="form-select">
+                                <option value=""></option>
+                                <?php
+                                foreach ($fruitSess as $fruit) {
+                                    echo "<option value='$fruit'>$fruit</option>";
+                                }
+                                ?>
+                            </select>
+                            <label for="selFruits">Select a fruit</label>
+                        </div>
+                        <div class="form-floating my-2">
+                            <input type="text" name="addFruit" id="addFruit" class="form-control" placeholder="Add">
+                            <label for="addFruit">Add a fruit</label>
+                        </div>
+                        <div class="form-floating my-2">
+                            <select name="delFruits" id="delFruits" class="form-select">
+                                <option value=""></option>
+                                <?php
+                                foreach ($fruitSess as $fruit) {
+                                    echo "<option value='$fruit'>$fruit</option>";
+                                }
+                                ?>
+                            </select>
+                            <label for="delFruits">Delete a fruit</label>
+                        </div>
+                        <div class="form-group my-3">
+                            <button type="submit" name="selectFruit" class="btn btn-success">Select</button>
+                            <button type="submit" name="addFruitBtn" class="btn btn-primary">Add</button>
+                            <button type="submit" name="deleteFruit" class="btn btn-danger">Delete</button>
+                        </div>
 
-                        <hr>
                         <?php
-                        if (isset($_POST['selFruits'])) {
+                        if (isset($_POST['selectFruit'])) {
                             $fruit = $_POST['selFruits'];
                             if ($fruit <> "") {
-                                echo "You have selected {$fruit}";
+                                echo '<div class="alert alert-success fade show" role="alert">
+                                      You have selected ' . $fruit .'
+                                      </div>';
                                 unset($_SESSION['errorSel']);
                             } else {
-                                echo $_SESSION['errorSel'] = "Please select a fruit.";
+                                echo $_SESSION['errorSel'] = '<div class="alert alert-danger fade show" role="alert">
+                                      Please select a fruit.
+                                      </div>';
                             }
                         }
-                        ?>
-                    </form>
-                </div>
-            </div>
-        </div>
 
-<!-- Add Fruits Card -->
-        <div class="col-sm-4">
-            <div class="card w-100">
-                <div class="card-body">
-                    <h3 class="card-title">Add Fruits</h3>
-                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                        <label for="addFruit">Add a fruit</label>
-                        <input type="text" name="addFruit" id="addFruit" class="form-control">
-                        <br>
-                        <input type="submit" value="Add" class="btn btn-primary">
-                        <hr>
-                        <?php
-                        if (isset($_POST['addFruit'])) {
+                        if (isset($_SESSION['addMsg'])) {
+                            echo '<div class="alert alert-warning fade show" role="alert">
+                                  ' . $_SESSION['addMsg'] . '
+                                  </div>';
+                            unset($_SESSION['addMsg']);
+                        }
+                        if (isset($_SESSION['delMsg'])) {
+                            echo '<div class="alert alert-danger fade show" role="alert">
+                                  ' . $_SESSION['delMsg'] . '
+                                  </div>';
+                            unset($_SESSION['delMsg']);
+                        }
+
+                        if (isset($_POST['addFruitBtn'])) {
                             $newFruit = $_POST['addFruit'];
                             if ($newFruit <> "") {
-                                echo "You have added {$newFruit}";
+                                $_SESSION['addMsg'] = "You have added {$newFruit}";
                                 array_push($fruitSess, $newFruit);
                                 $_SESSION['fruits'] = $fruitSess;
                                 header('Location: form.php');
                                 unset($_SESSION['errorAdd']);
                             } else {
-                                echo $_SESSION['errorAdd'] = "Please enter a fruit to add.";
+                                echo $_SESSION['errorAdd'] = '<div class="alert alert-danger fade show" role="alert">
+                                      Please enter a fruit to add.
+                                      </div>';
                             }
                         }
-                        ?>
-                    </form>
-                </div>
-            </div>
-        </div>
-
-<!--Delete Fruits-->
-        <div class="col-sm-4">
-            <div class="card w-100">
-                <div class="card-body">
-                    <h3 class="card-title">Delete Fruits</h3>
-                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-                        <label for="delFruits">Select a fruit to delete</label>
-                        <select name="delFruits" id="delFruits" class="form-control">
-                            <option value=""></option>
-                            <?php
-                            foreach ($fruitSess as $fruit) {
-                                echo "<option value='$fruit'>$fruit</option>";
-                            }
-                            ?>
-                        </select>
-                        <br>
-                        <input type="submit" value="Delete" class="btn btn-danger">
-
-                        <hr>
-                        <?php
-                        if (isset($_POST['delFruits'])) {
+                        if (isset($_POST['deleteFruit'])) {
                             $fruit = $_POST['delFruits'];
                             if ($fruit <> "") {
-                                echo "You have selected {$fruit}";
+                                $_SESSION['delMsg'] = "You have deleted {$fruit}";
                                 if (($key = array_search($fruit, $fruitSess)) !== false) {
                                     unset($fruitSess[$key]);
                                     $_SESSION['fruits'] = $fruitSess;
@@ -125,15 +121,26 @@ $fruitSess =  $_SESSION['fruits'];
                                 unset($_SESSION['errorDel']);
                                 header('Location: form.php');
                             } else {
-                                echo $_SESSION['errorDel'] = "Please select a fruit to delete.";
+                                echo $_SESSION['errorDel'] = '<div class="alert alert-danger fade show" role="alert">
+                                      Please select a fruit to delete.
+                                      </div>';
                             }
                         }
+
                         ?>
+                        <hr>
+                        <h3 class="card-title">Available Fruits:</h3>
+                        <ul>
+                            <?php
+                            foreach ($_SESSION['fruits'] as $fruit) {
+                                echo "<li>{$fruit}</li>";
+                            }
+                            ?>
+                        </ul>
                     </form>
                 </div>
             </div>
         </div>
-
     </div>
 </div>
 </body>
--- operation/fruitdata.php
+++ operation/fruitdata.php
@@ -4,12 +4,43 @@ session_start();
 $fruits = array("Mango", "Coconut", "Apple", "Orange");
 $_SESSION['fruits'] = $fruits;
 
-echo "List of fruits: <br>";
-foreach ($_SESSION['fruits'] as $fruit) {
-    echo "{$fruit} <br>";
-}
 
-$_SESSION ['errorSel'] = "Please select a fruit.";
-$_SESSION['errorAdd'] = "Please enter a fruit to add.";
-$_SESSION['errorDel'] = "Please select a fruit to delete.";
 ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fruit Data</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <script src="../js/bootstrap.bundle.min.js"></script>
+    <style>
+        body {
+            padding-top: 70px;
+            padding-bottom: 70px;
+            background-color: #eee;
+        }
+
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="col-sm-4">
+        <div class="card p-3">
+            <div class="card-body">
+                <h3 class="card-title">Available Fruits:</h3>
+                <ul>
+                    <?php
+                    foreach ($_SESSION['fruits'] as $fruit) {
+                        echo "<li>{$fruit}</li>";
+                    }
+                    ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
